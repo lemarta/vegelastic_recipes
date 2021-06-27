5f33bd3fda3b862834d17028f1ca4d2dc1af08fb
lemarta <marta.ras95@gmail.com>
2021-06-27 13:11:15 +0200

    Base template

--- Recipes/views.py
+++ Recipes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+
+class MainPageView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request=request, template_name='base.html', context=context)
--- Vegelastic_recipes/urls.py
+++ Vegelastic_recipes/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import Recipes.views as recipe_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', recipe_views.MainPageView.as_view()),
 ]
