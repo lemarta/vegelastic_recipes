2b4ee100351c8a1cab2af06e70764420f64995c8
lemarta <marta.ras95@gmail.com>
2021-07-01 20:41:29 +0200

    Models update

--- Recipes/models.py
+++ Recipes/models.py
@@ -4,22 +4,22 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 CATEGORY_CHOICES = (
-    (1, "śniadania"),
-    (2, "kolacje"),
-    (3, "obiady"),
-    (4, "lunche"),
-    (5, "desery"),
-    (6, "inne"),
+    (1, "breakfasts"),
+    (2, "suppers"),
+    (3, "dinners"),
+    (4, "lunches"),
+    (5, "desserts"),
+    (6, "other"),
 )
 
 MEASURE_CHOICES = (
-    (1, "łyżka"),
-    (2, "łyżeczka"),
-    (3, "szklanka"),
-    (4, "szczypta"),
+    (1, "teaspoon"),
+    (2, "tablespoon"),
+    (3, "glass"),
+    (4, "pinch"),
     (5, "gram"),
-    (6, "do smaku"),
-    (7, "sztuka")
+    (6, "to taste"),
+    (7, "unit")
 )
 
 
@@ -29,16 +29,19 @@ class User(AbstractUser):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=256, unique=True)
-    meal_description = models.TextField(max_length=2048, null=True, blank=True)
+    meal_description = models.TextField(blank=True)
     prep_time = models.PositiveSmallIntegerField(null=True, blank=True)
-    prep_instructions = models.TextField(max_length=2048, null=True, blank=True)
+    prep_instructions = models.TextField(blank=True)
+    has_servings = models.BooleanField()
+    servings = models.PositiveSmallIntegerField(null=True, blank=True)
+    servings_multiplier = models.PositiveSmallIntegerField(default=1)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=256)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(Recipe, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -65,32 +68,33 @@ class RecipeCategory(models.Model):
 
 
 class RecipeIngredient(models.Model):
-    ingredient_description = models.TextField(max_length=1024, null=True, blank=True)
+    ingredient_description = models.TextField(blank=True)
     is_searchable = models.BooleanField(default=False)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe_ingredient')
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe_ingredient')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe ingredients')
     measure = models.IntegerField(choices=MEASURE_CHOICES)
     amount = models.FloatField()
+    servings_multiplier = models.PositiveSmallIntegerField()
 
     @property
     def grammar_name(self):
-        if self.amount == 1:
+        if (self.amount * self.recipe.servings_multiplier) == 1:
             return self.ingredient.name_one
-        elif 2 <= self.amount <= 4:
+        elif 2 <= (self.amount * self.recipe.servings_multiplier) <= 4:
             return self.ingredient.name_two
-        elif self.amount >= 5:
+        elif (self.amount * self.recipe.servings_multiplier) >= 5:
             return self.ingredient.name_five
-        elif self.amount <= 1:
+        elif (self.amount * self.recipe.servings_multiplier) <= 1:
             return self.ingredient.name_half
         else:
             return self.ingredient.name
 
 
 class RecipeImage(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe_image')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe images')
     image = models.ImageField(upload_to='media/')
 
 
 class IngredientImage(models.Model):
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='ingredient_image')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='ingredient images')
     image = models.ImageField(upload_to=f'media/')
