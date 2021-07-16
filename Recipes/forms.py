ae5b224e4fa8eca0228344dc8270f034afb18d97
lemarta <marta.ras95@gmail.com>
2021-07-17 01:22:08 +0200

    Docstrings

--- Recipes/forms.py
+++ Recipes/forms.py
@@ -2,13 +2,15 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.forms import ModelForm
 
-from Recipes.models import Recipe, Ingredient
+from Recipes.models import Recipe
 
 User = get_user_model()
 
 
 class RecipeForm(ModelForm):
-
+    """
+    Creates a form based on Recipe model using specified fields
+    """
     class Meta:
         model = Recipe
         fields = ['name', 'meal_description', 'prep_time', 'prep_instructions', 'has_servings', 'servings']
