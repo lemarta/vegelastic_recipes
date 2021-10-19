d5b3ca9eba40008174c5c884cefbb9bd0c0441fc
lemarta <marta.ras95@gmail.com>
2021-10-19 21:33:01 +0200

    link styles change

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
         //$('#drop' , this).css('display','block');
         $(this).children('ul').delay(50).slideDown(500);
     }, function(){
-         $(this).children('ul').delay(50).slideUp(500);
+        $(this).children('ul').delay(50).slideUp(500);
     });
 });
